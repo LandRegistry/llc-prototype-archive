b78b0401664ef05f81c8ac64868e27de2b497885
Laura Criddle <laura.criddle@digital.landregistry.gov.uk>
2017-04-11 11:45:24 +0100

    page order changes, legislation search added

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,96 @@ var S = require('string');
 var app = express();
 router.use(bodyParser.urlencoded({ extended: false }));
 
+var legs = [
+  "Ancient Monuments and ArchaeologicalAreas Act 1979 (as amended…): s.1(9), s.2(3), s.8(6), s.12, s.16(8), s.33(5)",
+  "Highways Act 1980 (various sections)",
+  "Localism Act 2011 (Part 5, Chapter 3): The Assets of Community Value (England) 2012 SI 2012 No. 2421……",
+  "New Towns Act 1981: Order by Secretary of State - s.10 or s.11 (CPO by a Development Corporation)",
+  "Part 8 Regulation 66 Community Infrastructure Levy Regulations 2010",
+  "Part 8 Regulation 66 Community Infrastructure Levy Regulations 2011",
+  "Part XI Highways Act 1980 (under code of 1875)",
+  "s.106 Town and Country Planning Act 1990",
+  "s.107 Building Act 1984",
+  "s.107(9) Housing Act 1996",
+  "s.11(1) Opencast Coal Act 1958",
+  "s.119 Housing Act 1996",
+  "s.156(a) Housing Act 1985",
+  "s.36(a) Housing Act 1985",
+  "s.17 Land Powers (Defence) Act 1958",
+  "s.172 Town and Country Planning Act 1990",
+  "s.178(1)(b) Town and Country Planning Act 1990",
+  "s.18 Clean Air Act 1993",
+  "s.18(8) Land Drainage Act 1991",
+  "s.198 Town and Country Planning Act 1990 (when confirmed)",
+  "s.201 Town and Country Planning Act 1990 (immediate effect)",
+  "s.207 Town and Country Planning Act 1990 (RE replacement)",
+  "s.208 Town and Country Planning Act 1990 (appeal)",
+  "s.209 Town and Country Planning Act 1990 (enter land)",
+  "s.2(4) Rights of Light Act 1959",
+  "s.278(8) Highways Act 1980",
+  "s.28 Wildlife and Countryside Act 1981",
+  "s.38 Highways Act 1980",
+  "s.42(1) Planning (Listed Buildings and Conservation Areas) Act 1990",
+  "s.43 Civil Aviation Act 1982",
+  "s.44 Civil Aviation Act 1982",
+  "s.45 Civil Aviation Act 1982",
+  "s.46(1) Civil Aviation Act 1982",
+  "s.52(2) Housing Grants Construction and Regeneration Act 1996",
+  "s.52(8) Land Compensation Act 1973",
+  "s.69(4) Planning (Listed Buildings and Conservation Areas) Act 1990",
+  "s.70 Town and Country Planning Act 1990 (as amended)",
+  "s.8 Coast Protection Act",
+  "s.8(4) Land Compensation Act 1973",
+  "s2 Planning (Listed Buildings and Conservation Areas) Act 1990",
+  "Town and Country Planning (General Permitted Development) Order 1995",
+  "Various",
+  "No specific statutory provision"]
+
+var legs2 = [
+  ["s.1(9), s.2(3), s.8(6), s.12, s.16(8), s.33(5)", "Ancient Monuments and Archaeological Areas Act 1979 (as amended…)"],
+  ["various sections", "Highways Act 1980"],
+  ["(Part 5, Chapter 3)", "Localism Act 2011", "The Assets of Community Value (England) 2012 SI 2012 No. 2421……"],
+  ["s.10 or s.11", "New Towns Act 1981: Order by Secretary of State", "(CPO by a Development Corporation)"],
+  ["Part 8 Regulation 66", "Community Infrastructure Levy Regulations 2010", ""],
+  ["Part 8 Regulation 66", "Community Infrastructure Levy Regulations 2011", ""],
+  ["Part XI", "Highways Act 1980", "(under code of 1875)"],
+  ["s.106",  "Town and Country Planning Act 1990", ""],
+  ["s.107", "Building Act 1984", ""],
+  ["s.107(9)", "Housing Act 1996", ""],
+  ["s.11(1)", "Opencast Coal Act 1958", ""],
+  ["s.119", "Housing Act 1996", ""],
+  ["s.156(a)", "Housing Act 1985", ""],
+  ["s.36(a)", "Housing Act 1985", ""],
+  ["s.17", "Land Powers (Defence) Act 1958", ""],
+  ["s.172", "Town and Country Planning Act 1990", ""],
+  ["s.178(1)(b)", "Town and Country Planning Act 1990", ""],
+  ["s.18", "Clean Air Act 1993", ""],
+  ["s.18(8)", "Land Drainage Act 1991", ""],
+  ["s.198", "Town and Country Planning Act 1990", "(when confirmed)"],
+  ["s.201", "Town and Country Planning Act 1990", "(immediate effect)"],
+  ["s.207", "Town and Country Planning Act 1990", "(RE replacement)"],
+  ["s.208", "Town and Country Planning Act 1990", "(appeal)"],
+  ["s.209", "Town and Country Planning Act 1990", "(enter land)"],
+  ["s.2(4)", "Rights of Light Act 1959", ""],
+  ["s.278(8)", "Highways Act 1980", ""],
+  ["s.28", "Wildlife and Countryside Act 1981", ""],
+  ["s.38", "Highways Act 1980", ""],
+  ["s.42(1)", "Planning Act 1990", "(Listed Buildings and Conservation Areas)"],
+  ["s.43", "Civil Aviation Act 1982", ""],
+  ["s.44", "Civil Aviation Act 1982", ""],
+  ["s.45", "Civil Aviation Act 1982", ""],
+  ["s.46(1)", "Civil Aviation Act 1982", ""],
+  ["s.52(2)", "Housing Grants Construction and Regeneration Act 1996", ""],
+  ["s.52(8)", "Land Compensation Act 1973", ""],
+  ["s.69(4)", "Planning Act 1990", "(Listed Buildings and Conservation Areas)", ""],
+  ["s.70", "Town and Country Planning Act 1990", "(as amended)"],
+  ["s.8", "Coast Protection Act", ""],
+  ["s.8(4)", "Land Compensation Act 1973"],
+  ["s2", "Planning Act 1990", "(Listed Buildings and Conservation Areas)"],
+  ["Town and Country Planning Order 1995", "(General Permitted Development)"],
+  ["Various sections", "Or various acts", ""],
+  ["None", "No specific statutory provision", ""]]
+
 // Initialise express-session so we can do some fake sign stuff
 router.use(session({
   saveUninitialized: false,
@@ -184,5 +274,37 @@ router.all('/llc-03/register-:screen/address=:address', function (req, res) {
     });
 });
 
+router.all('/add_charge/v2-2/03_charge_search_results', function (req, res) {
+  searchTerm = req.body.legislation;
+  searchResult = inArray(searchTerm, legs2);
+  res.render('add_charge/v2-2/03_charge_search',
+  {'title': '', reply: searchResult
+  });
+});
+
+function filterItems(query) {
+   return legs.filter(function(el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+   })
+}
+
+function match(query, array) {
+   return array.filter(function(el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) > -1;
+   })
+}
+
+function inArray(query, outerArray) {
+    var lengthOuter = outerArray.length;
+    var matches = [];
+    for(var i = 0; i < lengthOuter; i++) {
+      innerArray = outerArray[i];
+      var matched = match(query, innerArray);
+      if (matched.length >= 1) {
+        matches.push(innerArray);
+      }
+    }
+    return matches;
+}
 
 module.exports = router;
