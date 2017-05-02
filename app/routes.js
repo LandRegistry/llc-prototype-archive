de364cf636898edb1b2e2b83a88b56d02e1d53a7
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2017-05-02 14:23:48 +0100

    Added shape upload journey into main

--- app/routes.js
+++ app/routes.js
@@ -307,12 +307,21 @@ function inArray(query, outerArray) {
     return matches;
 }
 
-router.get('/add_charge/v2-4_test/draw-charge-boundary', function (req, res) {
+router.get('/add_charge/v2-4/05_postcode', function (req, res) {
   var shape = req.query.shape
   if (shape === 'true') {
-    res.redirect('/add_charge/v2-4_test/check-charge-boundary')
+    res.redirect('/add_charge/v2-4/check-charge-boundary')
   } else {   
-    res.render('add_charge/v2-4_test/draw-charge-boundary')
+    res.render('add_charge/v2-4/05_postcode')
+  }
+})
+
+router.get('/add_charge/v2-4/check-charge-boundary', function (req, res) {
+  var shape = req.query.shape
+  if (shape === 'false') {
+    res.redirect('/add_charge/v2-4/05_postcode')
+  } else {   
+    res.render('add_charge/v2-4/check-charge-boundary')
   }
 })
 
