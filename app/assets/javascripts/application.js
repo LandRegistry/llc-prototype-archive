d8da5da34a43751957f618dace533c0916762b55
Haresh Vekriya <h.vekriya@kainos.com>
2017-07-12 09:09:36 +0100

    quick fix on js-hidden

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -101,7 +101,8 @@ $(document).ready(function () {
   // and focused states for block labels
   var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
   new GOVUK.SelectionButtons($blockLabels)
-
+  $('.js-hidden').css('display', 'none')
+  $('.js-hidden').attr('aria-hidden', 'true').hide()
   // Show and hide toggled content
   // Where .block-label uses the data-target attribute
   var toggleContent = new ShowHideContent()
