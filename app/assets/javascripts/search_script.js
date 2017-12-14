f288259842c9bddd390697f408b50e3a81b6679a
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2017-12-14 16:28:09 +0000

    Some js changes

--- app/assets/javascripts/search_script.js
+++ app/assets/javascripts/search_script.js
@@ -2,7 +2,10 @@ var loggedInCheck = sessionStorage.getItem('loggedIn');
 if (loggedInCheck === 'true') {
     $('.notLoggedIn').remove();
     $('#login-links').append(
-        '<li class="loggedIn"><a href="account" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Jane Doe</a></li>' +
+        '<li class="loggedIn"><a href="account" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Jane Doe</a></li>'
+        /* +
+                '<li class="loggedIn"><a href="dashboard" style="font-size:16px; text-decoration:none; color:white; font-weight: 700; margin-right: 1em;">Dashboard</a></li>'*/
+        +
         '<li class="loggedIn signOut"><a href="start" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Sign out</a></li>'
     );
     console.log('Logged in');
