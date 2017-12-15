606c3b3dc962ed573b58724072565522934d4871
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2017-12-15 09:24:27 +0000

    CSS rule added to remove inline styles in nav

--- app/assets/javascripts/search_script.js
+++ app/assets/javascripts/search_script.js
@@ -2,18 +2,18 @@ var loggedInCheck = sessionStorage.getItem('loggedIn');
 if (loggedInCheck === 'true') {
     $('.notLoggedIn').remove();
     $('#login-links').append(
-        '<li class="loggedIn"><a href="account" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Jane Doe</a></li>'
+        '<li class="loggedIn"><a href="account">Jane Doe</a></li>'
         /* +
                 '<li class="loggedIn"><a href="dashboard" style="font-size:16px; text-decoration:none; color:white; font-weight: 700; margin-right: 1em;">Dashboard</a></li>'*/
         +
-        '<li class="loggedIn signOut"><a href="start" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Sign out</a></li>'
+        '<li class="loggedIn signOut"><a href="start">Sign out</a></li>'
     );
     console.log('Logged in');
 } else if (loggedInCheck === 'false' || loggedInCheck === null) {
     $('.loggedIn').remove();
     $('#login-links').append(
-        '<li class="notLoggedIn"><a href="login" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Sign in</a></li>' +
-        '<li class="notLoggedIn"><a href="register" style="font-size:16px; text-decoration:none; color:white; font-weight: 700">Register</a></li>');
+        '<li class="notLoggedIn"><a href="login">Sign in</a></li>' +
+        '<li class="notLoggedIn"><a href="register">Register</a></li>');
 }
 
 $('.signOut').on('click', function() {
