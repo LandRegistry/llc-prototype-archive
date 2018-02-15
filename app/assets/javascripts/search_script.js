609d0fb23bbfa9996ee135ae3c3c494cc3324466
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-02-15 14:28:23 +0000

    Small fixes

--- app/assets/javascripts/search_script.js
+++ app/assets/javascripts/search_script.js
@@ -2,7 +2,7 @@ var loggedInCheck = sessionStorage.getItem('loggedIn');
 if (loggedInCheck === 'true') {
     $('.notLoggedIn').remove();
     $('#login-links').append(
-        '<li class="loggedIn"><a href="account">Jane Doe</a></li>'
+        '<li class="loggedIn"><a href="account">Jane Smith</a></li>'
         /* +
                 '<li class="loggedIn"><a href="dashboard" style="font-size:16px; text-decoration:none; color:white; font-weight: 700; margin-right: 1em;">Dashboard</a></li>'*/
         +
