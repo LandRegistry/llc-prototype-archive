cec7c0845deec292cfd60ba16831be64b9eccdfd
Gurur Sarbanoglu <thepurpletwilight@yahoo.co.uk>
2018-03-29 10:17:19 +0100

    New charge subcat page

--- app/assets/javascripts/stat-provs.js
+++ app/assets/javascripts/stat-provs.js
@@ -212,9 +212,93 @@ $('.list a').on('click', function() {
             break;
         default:
             $(this).attr('confirm-stat-prov-1-2-many-non-mand');
-    }
+    };
 
-})
+
+    
+
+});
+$('.alphabet-list a').on('click', function() {
+        const letter = $(this).attr('class');
+        switch (letter) {
+            case 'letter-control-a':
+                $('.letter').addClass('hiddenList');
+                $('.letter-a').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-b':
+                $('.letter').addClass('hiddenList');
+                $('.letter-b').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-c':
+                $('.letter').addClass('hiddenList');
+                $('.letter-c').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-e':
+                $('.letter').addClass('hiddenList');
+                $('.letter-e').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-f':
+                $('.letter').addClass('hiddenList');
+                $('.letter-f').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-g':
+                $('.letter').addClass('hiddenList');
+                $('.letter-g').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-h':
+                $('.letter').addClass('hiddenList');
+                $('.letter-h').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-i':
+                $('.letter').addClass('hiddenList');
+                $('.letter-i').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-l':
+                $('.letter').addClass('hiddenList');
+                $('.letter-l').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-n':
+                $('.letter').addClass('hiddenList');
+                $('.letter-n').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-p':
+                $('.letter').addClass('hiddenList');
+                $('.letter-p').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-r':
+                $('.letter').addClass('hiddenList');
+                $('.letter-r').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-s':
+                $('.letter').addClass('hiddenList');
+                $('.letter-s').removeClass('hiddenList');
+                return false;
+                break;
+            case 'letter-control-t':
+                $('.letter').addClass('hiddenList');
+                $('.letter-t').removeClass('hiddenList');
+                return false;
+                break;
+            default:
+                $('.letter').addClass('hiddenList');
+                $('.letter-a').removeClass('hiddenList');
+                return false;
+
+        }
+        
+    })
     
     
     
